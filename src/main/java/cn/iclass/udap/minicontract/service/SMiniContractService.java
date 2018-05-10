006d8c4d6a7016d6789470a01e4f3b817e3e2a0a
wwlleo0730 <leobluewing@qq.com>
2018-05-10 15:44:38 +0800

    暂时提交一版

--- src/main/java/cn/iclass/udap/minicontract/service/SMiniContractService.java
+++ src/main/java/cn/iclass/udap/minicontract/service/SMiniContractService.java
@@ -4,6 +4,7 @@ import javax.annotation.Resource;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import cn.iclass.udap.minicontract.domain.SMiniContract;
 import cn.iclass.udap.minicontract.repository.SMiniContractDao;
@@ -14,6 +15,67 @@ public class SMiniContractService {
 	
 	@Resource
 	private SMiniContractDao sMiniContractRepository;
+	
+	public void doEthereum(){
+		
+	}
+	
+	/**
+	 * 确认是否需要写入区块链
+	 * @param smini
+	 * @return
+	 */
+	boolean checkDoEthereum(SMiniContract smini){
+		
+		return false;
+	}
+	
+	
+	/**
+	 * 合同签字
+	 */
+	public void signContract(long contractId , String wxid){
+		
+		SMiniContract smini = 
+				this.sMiniContractRepository.findOne(contractId);
+		
+		String creator_wxid = smini.getCreator().getWxid();
+		
+		String receiver_wxid = smini.getReceiver().getWxid();
+		
+		//判断当前签字的身份
+		
+		if(creator_wxid.equals(wxid)){
+			//创建者签名
+			
+			if(checkDoEthereum(smini)){
+				
+				this.doEthereum();
+				
+				return ;
+				
+			}
+			
+		}
+		
+		if(receiver_wxid.equals(wxid)){
+			//被邀请者签名
+			
+			if(checkDoEthereum(smini)){
+				
+				this.doEthereum();
+				
+				return ;
+			}
+			
+		}
+		
+		//说明只有一个人签了字，不做处理
+		
+		
+		
+	}
+
 	
 	/**
 	 * 
