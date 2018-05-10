2846e9230a5d4ef5168d5c94d9fad4fb274f07ce
wwlleo0730 <leobluewing@qq.com>
2018-05-10 18:45:40 +0800

    从微信获取用户时，顺便新增信息

--- src/main/java/cn/iclass/udap/minicontract/wechat/WechatUserController.java
+++ src/main/java/cn/iclass/udap/minicontract/wechat/WechatUserController.java
@@ -1,5 +1,7 @@
 package cn.iclass.udap.minicontract.wechat;
 
+import javax.annotation.Resource;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,14 @@ import com.foxinmy.weixin4j.mp.api.OauthApi;
 import com.foxinmy.weixin4j.mp.model.OauthToken;
 import com.foxinmy.weixin4j.mp.model.User;
 
+import cn.iclass.udap.minicontract.domain.SAccount;
+import cn.iclass.udap.minicontract.repository.SAccountDao;
+
 @RestController
 public class WechatUserController {
+	
+	@Resource
+	private SAccountDao accountDao;
 	
 	@GetMapping("/wechat/user/{code}")
 	public User getUserByCode(@PathVariable String code){
@@ -27,6 +35,19 @@ public class WechatUserController {
 			
 			User user = weixinProxy.getUser(openId);
 			
+			//说明是新用户
+			if(null == accountDao.findByWxid(openId)){
+				
+				SAccount account = new SAccount();
+				
+				account.setAvatar(user.getHeadimgurl());
+				account.setLoginName(user.getNickName());
+				account.setWxid(openId);
+				
+				this.accountDao.save(account);
+				
+			}
+			
 			return user;
 			
 		} catch (WeixinException e) {
@@ -35,8 +56,6 @@ public class WechatUserController {
 		
 		
 		return new User();
-		
-		
 	}
 
 }
