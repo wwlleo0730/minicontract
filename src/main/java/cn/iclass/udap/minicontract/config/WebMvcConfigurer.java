9cb9fdb67a0f6e66840864ad08072b33143965d6
wwlleo0730 <leobluewing@qq.com>
2018-05-13 23:18:25 +0800

    使用http200的异常处理

---
+++ src/main/java/cn/iclass/udap/minicontract/config/WebMvcConfigurer.java
@@ -0,0 +1,108 @@
+package cn.iclass.udap.minicontract.config;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+
+import cn.iclass.udap.minicontract.core.Result;
+import cn.iclass.udap.minicontract.core.ResultCode;
+import cn.iclass.udap.minicontract.core.ServiceException;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+/**
+ * Spring MVC，主要用来解决统一异常问题
+ */
+@Configuration
+public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
+
+    private final Logger logger = LoggerFactory.getLogger(WebMvcConfigurer.class);
+   
+    //使用阿里 FastJson 作为JSON MessageConverter
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        FastJsonConfig config = new FastJsonConfig();
+        config.setSerializerFeatures(SerializerFeature.WriteMapNullValue,//保留空的字段
+                SerializerFeature.WriteNullStringAsEmpty,//String null -> ""
+                SerializerFeature.WriteNullNumberAsZero);//Number null -> 0
+        converter.setFastJsonConfig(config);
+        converter.setDefaultCharset(Charset.forName("UTF-8"));
+        converters.add(converter);
+    }
+
+
+    //统一异常处理
+    @Override
+    public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
+        exceptionResolvers.add(new HandlerExceptionResolver() {
+            public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception e) {
+                Result result = new Result();
+                if (e instanceof ServiceException) {//业务失败的异常，如“账号或密码错误”
+                    result.setCode(ResultCode.FAIL).setMessage(e.getMessage());
+                    logger.info(e.getMessage());
+                } else if (e instanceof NoHandlerFoundException) {
+                    result.setCode(ResultCode.NOT_FOUND).setMessage("接口 [" + request.getRequestURI() + "] 不存在");
+                } else if (e instanceof ServletException) {
+                    result.setCode(ResultCode.FAIL).setMessage(e.getMessage());
+                } else {
+                    result.setCode(ResultCode.INTERNAL_SERVER_ERROR).setMessage("接口 [" + request.getRequestURI() + "] 内部错误，请联系管理员");
+                    String message;
+                    if (handler instanceof HandlerMethod) {
+                        HandlerMethod handlerMethod = (HandlerMethod) handler;
+                        message = String.format("接口 [%s] 出现异常，方法：%s.%s，异常摘要：%s",
+                                request.getRequestURI(),
+                                handlerMethod.getBean().getClass().getName(),
+                                handlerMethod.getMethod().getName(),
+                                e.getMessage());
+                    } else {
+                        message = e.getMessage();
+                    }
+                    logger.error(message, e);
+                }
+                responseResult(response, result);
+                return new ModelAndView();
+            }
+
+        });
+    }
+
+
+    private void responseResult(HttpServletResponse response, Result result) {
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Content-type", "application/json;charset=UTF-8");
+        response.setStatus(200);
+        try {
+            response.getWriter().write(JSON.toJSONString(result));
+        } catch (IOException ex) {
+            logger.error(ex.getMessage());
+        }
+    }
+
+
+}
